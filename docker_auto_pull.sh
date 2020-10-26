ea087960c15e28a87de869a866012d1a7b36b761
Jiri Hubacek <hubacekjirka@gmail.com>
2020-10-26 23:31:24 +1100

    done

---
+++ docker_auto_pull.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+# Change directory to the directory of the script ==> where docker-compose file is
+cd "$(dirname "$0")"
+
+echo "$(date +"%Y-%m-%d_%H-%M-%S") Scipt started" >> docker_auto_pull.log 2>&1
+
+docker pull hubacekjirka/s3-telegram-uploader:latest >> docker_auto_pull.log 2>&1
+
+docker-compose up -d --remove-orphans >> docker_auto_pull.log 2>&1
+
+echo "$(date +"%Y-%m-%d_%H-%M-%S") Scipt finished" >> docker_auto_pull.log 2>&1
